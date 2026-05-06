11226a637d9af03adace37af4990c32e6b6f003c
Malthe <maaa0001@stud.kea.dk>
2026-05-06 11:55:18 +0200

    update css and js with filtering

--- static/app.js
+++ static/app.js
@@ -1,68 +1,36 @@
 function clear_markers(data) {
-  console.log(data);
-  const urlParams = new URLSearchParams(window.location.search);
-  // urlParams.set('beds', '2')
-  // urlParams.set('baths', '1')
-  //window.history.replaceState({}, '', `?${urlParams.toString()}`)
-
-  // ?beds=2&baths=1
+  console.log("clear_markers called with data:", data);
+  const urlParams = new URLSearchParams();
 
   markers.clearLayers();
   data = JSON.parse(data);
   console.log(data.url);
   data.url.forEach((prop) => {
-    console.log(Object.keys(prop)[0]); // beds | baths
-    let key = Object.keys(prop)[0]; // beds | baths
-    console.log(prop[key]); //
-    let value = prop[key]; // 2 | 1
+    let key = Object.keys(prop)[0];
+    let value = prop[key];
     urlParams.set(key, value);
   });
   window.history.replaceState({}, "", `?${urlParams.toString()}`);
 
-  // items = data.items
-  // console.log(items)
   data.items.forEach((item) => {
-    if (item.type == "villa") {
-      var marker = L.marker([item.lat, item.lon], {
-        icon: L.divIcon({
-          className: "",
-          html: `
+    var marker = L.marker([item.item_lat, item.item_lon], {
+      icon: L.divIcon({
+        className: "",
+        html: `
                         <button 
-                            class="marker ${item.type}" onclick="mixhtml(); return false;"
-                            mix-get="api-get-item?item_pk=${item.pk}">
+                            class="marker ${item.item_type}" onclick="mixhtml(); return false;"
+                            mix-get="api-get-item?item_pk=${item.item_pk}">
                         </button>
                     `,
-        }),
-        item_pk: item.pk,
-      });
-    } else if (item.type == "condo") {
-      var marker = L.marker([item.lat, item.lon], {
-        icon: L.divIcon({
-          className: "",
-          html: `
-                        <button 
-                            class="marker ${item.type}" onclick="mixhtml(); return false;"
-                            mix-get="api-get-item?item_pk=${item.pk}">
-                        </button>
-                    `,
-        }),
-        item_pk: item.pk,
-      });
-    } else {
-      var marker = L.marker([item.lat, item.lon], {
-        icon: L.divIcon({
-          className: "",
-          html: `
-                        <button 
-                            class="marker ${item.type}" onclick="mixhtml(); return false;"
-                            mix-get="api-get-item?item_pk=${item.pk}">
-                        </button>
-                    `,
-        }),
-        item_pk: item.pk,
-      });
-    }
+      }),
+      item_pk: item.item_pk,
+    });
     markers.addLayer(marker);
   });
   map.addLayer(markers);
+
+  // Zoom to fit the markers if there are any
+  if (data.items.length > 0) {
+    map.fitBounds(markers.getBounds(), { padding: [50, 50], maxZoom: 15 });
+  }
 }
