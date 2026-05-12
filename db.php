ebf05dec928f006dedd2bf22d36784a3d28c8d3b
Malthe <maaa0001@stud.kea.dk>
2026-05-12 15:09:00 +0200

    add local fix and debugging

--- db.php
+++ db.php
@@ -1,12 +1,19 @@
 <?php
 // Check if we are on InfinityFree or Localhost
-$is_localhost = ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['REMOTE_ADDR'] === '127.0.0.1');
+$is_localhost = ($_SERVER['HTTP_HOST'] === 'localhost' || 
+                 $_SERVER['REMOTE_ADDR'] === '127.0.0.1' || 
+                 (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false) ||
+                 strpos('DB_HOST_PLACEHOLDER', 'PLACEHOLDER') !== false); 
 
 if ($is_localhost) {
     $dbUserName = 'root';
     $dbPassword = 'password';
     $dbConnection = 'mysql:host=mariadb; dbname=power_house_portal; charset=utf8mb4';
 } else {
+    // Show errors on InfinityFree for debugging purposes
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     $dbUserName = 'DB_USERNAME_PLACEHOLDER';
     $dbPassword = 'DB_PASSWORD_PLACEHOLDER';
     $dbConnection = 'mysql:host=DB_HOST_PLACEHOLDER; dbname=DB_NAME_PLACEHOLDER; charset=utf8mb4';
